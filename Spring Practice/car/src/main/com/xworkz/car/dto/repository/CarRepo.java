23ac1efe4551b7c223c68281bbba5165be77858d
Meghna G Ninganur <ninganurmeghna@gmail.com>
2025-08-02 11:05:36 +0530

    Delete Spring Practice/car/src/main directory

--- Spring Practice/car/src/main/com/xworkz/car/dto/repository/CarRepo.java
+++
@@ -1,8 +0,0 @@
-package main.com.xworkz.car.dto.repository;
-
-import main.com.xworkz.car.dto.CarDto;
-
-public interface CarRepo {
-    boolean peristant(CarDto carDto);
-
-}
--- Spring Practice/car/src/main/com/xworkz/car/dto/service/CarService.java
+++
@@ -1,7 +0,0 @@
-package main.com.xworkz.car.dto.service;
-
-import main.com.xworkz.car.dto.CarDto;
-
-public interface CarService {
-    boolean verfication(CarDto carDto);
-}
