9e362dc1d11239ff419b00dfb3bc6d3cb7344288
Meghna G Ninganur <ninganurmeghna@gmail.com>
2025-08-02 10:51:41 +0530

    Delete Ice-Cream (2) directory

--- Ice-Cream (2)/src/main/java/com/xworkz/icecream/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.icecream.service;
-
-import com.xworkz.icecream.dto.OrderDTO;
-
-public interface OrderService {
-    boolean save(OrderDTO orderDTO);
-    double calculateTotal(OrderDTO orderDTO);
-}
