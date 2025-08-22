9aa6dfd811b816999353df818eb80daca66c750b
Meghna-08-12-2002 <ninganurmeghna@gmail.com>
2025-08-22 10:33:29 +0530

    Commit Daily task-02

---
+++ DataBloom/Daily practice day-01/src/SumOfDigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int sum = 0, temp = num;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+    }
+}
